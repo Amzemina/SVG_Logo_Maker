de3c6bbb606a63fe932430d31d97764fe537f671
Ashley Zemina <amzemina@gmail.com>
2023-08-13 18:19:40 -0400

    added prompts to index js, added testing code for shapes and colors, added color var in shapes js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//Imports
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Circle, Square, Triangle} = require("./lib/shapes");
+const shape = require("./lib/shapes");
+
+
+//function to prompt user for logo details
+function promptUser() {
+    inquirer
+      .prompt([
+        // Text prompt
+        {
+          type: "input",
+          message: "What text would you like you logo to display?: Enter three characters",
+          name: "text",
+        },
+        // Text color prompt
+        {
+          type: "input",
+          message:  "Choose text color: Enter color keyword OR a hexadecimal number",
+          name: "textColor",
+        },
+        // Shape choice prompt
+        {
+          type: "list",
+          message: "What shape would you like the logo to be?",
+          choices: ["Circle", "Square", "Triangle"],
+          name: "shape",
+        },
+        // Shape color prompt
+        {
+          type: "input",
+          message: "Choose shape color: Enter color keyword OR a hexadecimal number",
+          name: "shapeColor",
+        },
+      ])
+    }
+    promptUser()
